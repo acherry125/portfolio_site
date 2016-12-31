b5f4045ae8d2d1d883e0c4d03e1ae9e402a8f46c
Alex Cherry <Alex Cherry>
2016-12-30 21:27:54 -0600

    added error path handling

--- scripts/app.js
+++ scripts/app.js
@@ -2,4 +2,16 @@
 // passing our module the express module
 module.exports = function(app) {
 
+	app.get(['/', '/home'], renderHomePage);
+	app.get('*', renderErrorPage);
+
+	function renderHomePage(req, res) {
+		res.status(200).sendFile('index.html', {root: __dirname + '/../public/' });
+	}
+
+	function renderErrorPage(req,res) {
+		res.status(404).sendFile('index.html', {root: __dirname + '/../public/' });
+	};
+
+
 };
